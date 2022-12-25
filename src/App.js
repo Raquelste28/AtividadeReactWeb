f3f18f0aafab6532286d3f60203694e804b16555
Raquelste28 <raquelsteslva@gmail.com>
2022-12-25 15:52:26 -0300

    comitando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ width: 300, marginTop: 100 }} >
+      <Form >
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" />
+          <Form.Text className="text-muted">
+
+          </Form.Text>
+        </Form.Group>
+      </Form>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="Senha" />
+        </Form.Group>
+      </Form>
+      <Button style={{ width: 275, color: "white", backgroundColor: "#0e6cf7" }} variant="button">Acessar</Button>{' '}
+
+    </Container>
+
   );
 }
 
